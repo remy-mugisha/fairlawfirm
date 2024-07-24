7a60eb5c1cb04d41f0122959763d3fb9bf26b399
remy-mugisha <remymugisha64@gmail.com>
2024-07-24 10:33:42 +0200

    add changes

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- ========== Meta Tags ========== -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="bracket-web">
     <meta name="description" content="Firdip is beautifully designed Figma template especially for the fire department, fireman, fire prevention, fire fighting, fire station, protection, firefighter and all other fire & safety business and websites.">
 
-    <!-- ======== Page title ============ -->
     <title>Fair Law Firm LTD</title>
 
     <?php 
